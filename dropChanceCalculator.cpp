5af4a40cbff1b33031a047b698de1042d08304f7
rosinantecora <flora.482613150075@depedqc.ph>
2024-12-01 16:22:26 +0800

    sibat

--- dropChanceCalculator.cpp
+++ dropChanceCalculator.cpp
@@ -1,6 +1,5 @@
 //dropChanceCalculator.cpp
 #include "header1.h"
-#include "header2.h"
 
 double dropChanceCalc(double dropRate, int attempts) {
     double chance = 1 - pow(1 - dropRate, attempts);
